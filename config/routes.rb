d62d94d27e3b3d16591cd3022f2d5ef89d960f33
tafdev <chombot@hotmail.com>
2016-06-30 13:10:07 +0000

    update views and nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => "user/registrations" }
   resources :user_stocks, except: [:edit, :show, :update]
   root 'welcome#index'
   get 'my_portfolio', to: "users#my_portfolio"
